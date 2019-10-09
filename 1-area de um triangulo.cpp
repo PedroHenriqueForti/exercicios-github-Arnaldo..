69e1a1fa7e02acadd90fa1fa2f002fb8d26fef80
Pedro Forti 1.DS <50830591+PedroHenriqueForti@users.noreply.github.com>
2019-10-09 11:23:52 -0300

    Add files via upload

---
+++ 1-area de um triangulo.cpp
@@ -0,0 +1,25 @@
+/* programa:
+nome:Pedro Henrique Silva Forti.
+data: 9/10/2019 */
+
+
+#include<stdio.h>
+
+int main () { 
+
+   float altura, base;
+  		 printf( "Informe a base: " );
+   scanf( "%i", &base );
+   		printf( "informe a altura: " );
+   scanf( "%i", &altura );
+
+   		printf( "area do triangulo e igual a : %f", base * altura / 2 );
+
+return 0;
+
+}
+
+
+
+
+
